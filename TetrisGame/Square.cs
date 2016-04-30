b5bcc700e84eae2cea93f3e30e13d6f9d82816fa
Martina <martina.tosevska.95@gmail.com>
2016-04-30 15:39:18 +0200

    Add Square class

---
+++ TetrisGame/Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame
+{
+    public class Square
+    {
+        public static float width = 20;
+        public static float height = 20;
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Color color { get; set; }
+        public bool canMove { get; set; }
+
+        public Square(float x, float y, Color c)
+        {
+            X = x;
+            Y = y;
+            color = c;
+            canMove = true;
+        }
+    }
+}
