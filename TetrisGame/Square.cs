e7ad00fee61972cba098e65f00258558b5b381e6
Martina <martina.tosevska.95@gmail.com>
2016-05-02 20:35:52 +0200

    Add methods in Square.cs and Tetrimino.cs

--- TetrisGame/Square.cs
+++ TetrisGame/Square.cs
@@ -9,19 +9,41 @@ namespace TetrisGame
 {
     public class Square
     {
-        public readonly float width = 20;
-        public readonly float height = 20;
+        public readonly float size = 20;
         public float X { get; set; }
         public float Y { get; set; }
         public Color color { get; set; }
         public bool canMove { get; set; }
+        public Point location { get; set; }
 
-        public Square(float x, float y, Color c)
+        public Square(float x, float y, Color c, Point l)
         {
             X = x;
             Y = y;
             color = c;
             canMove = true;
+            location = l;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(color), location.X + X * size, location.Y + Y * size, size, size);
+            g.DrawRectangle(new Pen(Color.RosyBrown), location.X + X * size, location.Y + Y * size, size, size);
+        }
+
+        public void moveDown()
+        {
+            Y++;
+        }
+
+        public void moveLeft()
+        {
+            X--;
+        }
+
+        public void moveRight()
+        {
+            X++;
         }
     }
 }
--- TetrisGame/Tetrimino.cs
+++ TetrisGame/Tetrimino.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,39 @@ namespace TetrisGame
 {
     public class Tetrimino
     {
+        public Square s1 { get; set; }
+        public Square s2 { get; set; }
+        public Square s3 { get; set; }
+        public Square s4 { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Color color { get; set; }
+
+        public Tetrimino(Point p, float x, float y)
+        {
+            X = x;
+            Y = y;
+            color = Color.PeachPuff;
+        }
+
+        public void Draw(Graphics g)
+        {
+
+        }
+
+        public void moveDown()
+        {
+            
+        }
+
+        public void moveLeft()
+        {
+            
+        }
+
+        public void moveRight()
+        {
+            
+        }
     }
 }
